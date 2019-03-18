d2f17f78ebec3db06a6b9c5c5519ecec90adb756
wanghaiyang <wqzbxh@163.com>
2019-03-18 19:45:43 +0800

    聊天室代码初始化

---
+++ js/index.js
@@ -0,0 +1,10 @@
+document.onkeydown=function(e){
+    if(e.keyCode == 13 && e.ctrlKey){ // 这里实现换行
+        document.getElementById("chatContent").value += "\n";
+    }else if(e.keyCode == 13){// 避免回车键换行
+        e.preventDefault();
+        // 下面写你的发送消息的代码
+		
+    }
+}
+
