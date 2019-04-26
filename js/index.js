206e999aefec76cec2eb456b7b45ed98af0b0ced
wanghaiyang <wqzbxh@163.com>
2019-04-26 21:20:36 +0800

    登录提交提交

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-
+	document.onkeydown=function(e){
+	    var content =  document.getElementById("chatContent").value;
+	    if(e.keyCode == 13 && e.ctrlKey){ // 这里实现换行
+	        document.getElementById("chatContent").value += "\n";
+	    }else if(e.keyCode == 13){// 避免回车键换行
+	        e.preventDefault();
+	        // 下面写你的发送消息的代码
+			var msg_obj = {"action_type":"senAllMsg","username":username,"userid":userid,"content":content};
+	 		var msg = JSON.stringify(msg_obj);
+	        if(content!= ""){
+	            ws.send(msg);
+				var text="";
+				document.getElementById("chatContent").value = text;
+	        }
+	    }
+	}
